d19cd17f093626a7d7d1cd345f6bd543d4bd542e
Wilko Quak <w.quak@geonovum.nl>
2023-07-06 00:19:49 +0200

    added comment

--- process-template.sh
+++ process-template.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#
+# Script to copy template files to local repository. Remove any file where you wish to keep the local changes.
+#
 cat ../dso-cim-template/README.md.template | sed -f configuration.sed > README.md
 cat ../dso-cim-template/js/config.js.template | sed -f configuration.sed > js/config.js
 cat ../dso-cim-template/imvertor/Imvertor.properties.template | sed -f configuration.sed > imvertor/Imvertor.properties
